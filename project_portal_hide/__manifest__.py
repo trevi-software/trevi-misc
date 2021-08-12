cd89b2547221e72ca8236f911ff399d0a26e1628
Michael Telahun <mtm@trevi.et>
2021-08-12 10:53:51 +0300

    [IMP] Add app icon and cover image; replace &amp; with 'and'

--- project_portal_hide/__manifest__.py
+++ project_portal_hide/__manifest__.py
@@ -2,7 +2,7 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 {
-    "name": "Website Hide Projects &amp; Tasks",
+    "name": "Website Hide Projects and Tasks",
     "version": "14.0.1.0.0",
     "license": "AGPL-3",
     "author": """TREVI Software,
@@ -10,8 +10,10 @@
     "summary": """Hide projects &amp; tasks in the customer portal.""",
     "category": "Project Management",
     "maintainers": ["TREVI Software"],
+    "images": ["static/src/img/main_screenshot.png"],
     "website": "https://github.com/trevi-software/trevi-misc",
     "depends": [
+        "portal",
         "project",
     ],
     "data": [
