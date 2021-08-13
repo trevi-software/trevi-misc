2042f1b2b9b607bbe475e4bad82dd1cd172d2cfb
Michael Telahun <mtm@trevi.et>
2021-08-13 15:36:36 +0300

    rename again: it_infrastructure -> itm (much shorter, easier to use)

---
+++ itm/__init__.py
@@ -0,0 +1,8 @@
+# Copyright (C) 2021 TREVI Software
+# Copyright (C) 2014 Leandro Ezequiel Baldi
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+# flake8: noqa
+from . import models
+from . import wizard
+from . import tests
