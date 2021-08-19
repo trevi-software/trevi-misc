21b50494f1dfa70397b1c494a7442176f551c9e8
Michael Telahun <mtm@trevi.et>
2021-08-19 13:59:43 +0300

    web_clipboard_password: use Clipboard.js and remove kanban support

--- web_clipboard_password/static/src/js/web_clipboard_password.js
+++ web_clipboard_password/static/src/js/web_clipboard_password.js
@@ -1,5 +1,6 @@
 "use strict";
 /**
+ * Copyright 2021 TREVI Software <support@trevi.et>
  * Copyright 2018 Modoolar <info@modoolar.com>
  * License LGPLv3.0 or later (https://www.gnu.org/licenses/lgpl-3.0.en.html).
  *
@@ -7,37 +8,53 @@
 
 odoo.define('web_clipboard_password', function (require) {
     const InputField = require("web.basic_fields").InputField;
-    const setClipboard = require("web_clipboard_password.set_clipboard");
     const field_registry = require('web.field_registry');
     const core = require('web.core');
+    const rpc = require('web.rpc')
     const _t = core._t;
 
     InputField.include({
         events: Object.assign({}, InputField.prototype.events, {
-            'click .copy-to-clipboard': '_onCopyToClipboardClick',
+            'click .o_clipboard_button': '_onCopyToClipboardClick',
         }),
         init() {
             this._super.apply(this, arguments);
             this.nodeOptions.isCopyable = 'copyable' in this.attrs;
+
         },
         _renderEdit: function () {
             this._super.apply(this, arguments);
             if (this.nodeOptions.isCopyable) {
-                this.$el.append('<a class="btn btn-default copy-to-clipboard" href="#"><i class="fa fa-copy"/></a>');
+                this.$el.append('<a class="btn btn-default o_clipboard_button" href="#"><i class="fa fa-clipboard"/></a>');
             }
         },
         _renderReadonly() {
             this._super.apply(this, arguments);
             if (this.nodeOptions.isCopyable) {
-                this.$el.append('<a class="btn btn-default copy-to-clipboard" href="#"><i class="fa fa-copy"/></a>');
+                this.$el.append('<a class="btn btn-default o_clipboard_button" href="#"><i class="fa fa-clipboard"/></a>');
             }
         },
         _onCopyToClipboardClick(event) {
             event.preventDefault();
             event.stopPropagation();
-            const value = this.$input ? this.$input.attr("value") : this.value;
-            setClipboard(value);
-            this.do_notify(_t("Copied to clipboard"));
+            var my_id = this.record.data.id;
+            var do_notify = this.do_notify;
+            var $clipboardBtn = this.$el.find('.o_clipboard_button');
+
+            rpc.query({
+                model: 'itm.access',
+                method: 'decrypt_password_as_string',
+                args: [my_id]
+            }).then(function (plaintext) {
+                var clipboard = new ClipboardJS($clipboardBtn[0], {
+                    text: function (trigger) {
+                        return plaintext;
+                    }
+                });
+                clipboard.onClick(event)
+                clipboard.destroy();
+                do_notify(_t("Copied password to clipboard"));
+            });
         }
     });
     const CopyableInput = InputField.extend({
