a0cdfd1afe32ec2102a62df1bac5b1cb8b0bb558
Michael Telahun <mtm@trevi.et>
2021-11-18 22:35:37 +0300

    [ADD] partner_contact_it: designate a contact as an IT contact
    This module allows the user to designate a contact as an
    IT Contact for the parent organization. This is a base module and
    is meant to be used by other modules.

---
+++ partner_contact_it/__manifest__.py
@@ -0,0 +1,15 @@
+# Copyright (C) 2021 TREVI Software
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    "name": "IT Contact",
+    "summary": "Designate a contact as an IT Contact of the parent company",
+    "version": "14.0.1.0.0",
+    "category": "CRM",
+    "license": "AGPL-3",
+    "author": "TREVI Software",
+    "website": "https://github.com/trevi-software/trevi-misc",
+    "depends": ["base"],
+    "data": ["views/res_partner_view.xml"],
+    "installable": True,
+}
