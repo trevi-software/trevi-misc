cb5a2845b84b359ff13d396ff21ac552aa66bf32
Michael Telahun <mtm@trevi.et>
2022-09-04 14:56:26 +0300

    itm: auto-migrate network_id, improve ip address view in itm.site.network

--- itm/__init__.py
+++ itm/__init__.py
@@ -1,4 +1,4 @@
-# Copyright (C) 2021 TREVI Software
+# Copyright (C) 2021,2022 TREVI Software
 # Copyright (C) 2014 Leandro Ezequiel Baldi
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
