bdd1c8899ed0b217a2bbfae5d8b4fa6e2ee66c31
Michael Telahun <mtm@trevi.et>
2022-10-15 19:59:27 +0300

    [IMP] itm: black, isort, prettier

--- itm/static/src/js/itm.js
+++ itm/static/src/js/itm.js
@@ -5,53 +5,55 @@
  */
 
 odoo.define("itm", function (require) {
-    "use strict";
+  "use strict";
 
-    const widgetRegistry = require("web.widget_registry");
-    const core = require("web.core");
-    const rpc = require("web.rpc");
-    const Widget = require("web.Widget");
-    const _t = core._t;
+  const widgetRegistry = require("web.widget_registry");
+  const core = require("web.core");
+  const rpc = require("web.rpc");
+  const Widget = require("web.Widget");
+  const _t = core._t;
 
-    var CopyToClipboard = Widget.extend({
-        template: "Copy_to_Clipboard",
-        xmlDependencies: ["/itm/static/src/xml/clipboard.xml"],
-        events: {
-            click: "_onClick",
-        },
+  var CopyToClipboard = Widget.extend({
+    template: "Copy_to_Clipboard",
+    xmlDependencies: ["/itm/static/src/xml/clipboard.xml"],
+    events: {
+      click: "_onClick",
+    },
 
-        init: function (parent, record, nodeInfo) {
-            this._super.apply(this, arguments);
-            this.res_id = record.res_id;
-            this.res_model = record.model;
-            this.node = nodeInfo;
-        },
+    init: function (parent, record, nodeInfo) {
+      this._super.apply(this, arguments);
+      this.res_id = record.res_id;
+      this.res_model = record.model;
+      this.node = nodeInfo;
+    },
 
-        _onClick(event) {
-            event.preventDefault();
-            event.stopPropagation();
-            var do_notify = this.do_notify;
-            var $clipboardBtn = this.$el.find(".o_clipboard_button");
+    _onClick(event) {
+      event.preventDefault();
+      event.stopPropagation();
+      var do_notify = this.do_notify;
+      var $clipboardBtn = this.$el.find(".o_clipboard_button");
 
-            rpc.query({
-                model: this.res_model,
-                method: "decrypt_password_as_string",
-                args: [this.res_id],
-            }).then(function (plaintext) {
-                // eslint-disable-next-line no-undef
-                var clipboard = new ClipboardJS($clipboardBtn[0], {
-                    text: function () {
-                        return plaintext;
-                    },
-                });
-                clipboard.onClick(event);
-                clipboard.destroy();
-                do_notify(_t("Copied password to clipboard"));
-            });
-        },
-    });
+      rpc
+        .query({
+          model: this.res_model,
+          method: "decrypt_password_as_string",
+          args: [this.res_id],
+        })
+        .then(function (plaintext) {
+          // eslint-disable-next-line no-undef
+          var clipboard = new ClipboardJS($clipboardBtn[0], {
+            text: function () {
+              return plaintext;
+            },
+          });
+          clipboard.onClick(event);
+          clipboard.destroy();
+          do_notify(_t("Copied password to clipboard"));
+        });
+    },
+  });
 
-    widgetRegistry.add("copy_to_clipboard", CopyToClipboard);
+  widgetRegistry.add("copy_to_clipboard", CopyToClipboard);
 
-    return CopyToClipboard;
+  return CopyToClipboard;
 });
