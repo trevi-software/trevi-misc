6098ba63ecab4c4182cd4c1083bface468bd778f
Michael Telahun <mtm@trevi.et>
2022-10-15 19:57:54 +0300

    [MIG] partner_contact_it: Migration to 15.0

--- partner_contact_it/__manifest__.py
+++ partner_contact_it/__manifest__.py
@@ -1,10 +1,10 @@
-# Copyright (C) 2021 TREVI Software
+# Copyright (C) 2021,2022 TREVI Software
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 {
     "name": "IT Contact",
     "summary": "Designate a contact as an IT Contact of the parent company",
-    "version": "14.0.1.0.0",
+    "version": "15.0.1.0.0",
     "category": "CRM",
     "license": "AGPL-3",
     "author": "TREVI Software",
